1c708bd9e2fb7735ba7078f026837792fd84aa4e
Vivek Singh <vvksngh100@gmail.com>
2026-06-23 18:56:52 +0530

    First commit

---
+++ Python/datatype_numeric.py
@@ -0,0 +1,29 @@
+age = 21
+year = 2026
+
+print("Age : ", age)
+print("Year : ", year)
+
+# Airthmetic opertions
+print("Age + 1 : ", age + 1)
+print("Year + 1 : ", year + 1)
+
+
+# Floating numbers
+price = 99.99
+temperature = -3.5
+
+print("Price : ", price)
+print("Temperature : ", temperature)
+
+# Float arithmetic operation
+print("Half Price : ", price/2)
+print("Increase temp by 5 : ", temperature + 5)
+
+
+# Boolean type
+is_student = True
+has_paused = True
+
+print("Is student : ", is_student)
+print("Has paused : ", has_paused)
---
+++ Python/datatype_string.py
@@ -0,0 +1,13 @@
+name = "Vivek Singh"
+greeting = "Hello World!"
+
+print("name ", name);
+print("greetings ", greeting);
+
+# convert to upper and lower case
+print("name in uppercase ", name.upper());
+print("name in lowercase ", name.lower());
+
+# Get the length of a string
+print("Length of the string ", len(name));
+print("Length of greetng is ", len(greeting));
